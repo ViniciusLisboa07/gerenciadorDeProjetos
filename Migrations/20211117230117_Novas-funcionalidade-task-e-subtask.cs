2d7f0db9fbb3f7cea7722a4f15468b71215b93a4
Maria Gabriela <mgabriela.mgs@outlook.com>
2021-11-17 20:04:32 -0300

    Rodando Migrations da base

---
+++ Migrations/20211117230117_Novas-funcionalidade-task-e-subtask.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GerenciadorDeProjetos.Migrations
+{
+    public partial class Novasfuncionalidadetaskesubtask : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "End",
+                table: "Task",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateTable(
+                name: "Subtask",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TaskId = table.Column<int>(type: "int", nullable: false),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    End = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subtask", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Subtask_Task_TaskId",
+                        column: x => x.TaskId,
+                        principalTable: "Task",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Subtask_TaskId",
+                table: "Subtask",
+                column: "TaskId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Subtask");
+
+            migrationBuilder.DropColumn(
+                name: "End",
+                table: "Task");
+        }
+    }
+}
